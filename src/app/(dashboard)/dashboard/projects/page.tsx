5ac8b6efc892e50cd9663f8be6d0bc373d2cfd96
rubelrana123 <rubelranatpi707458@gmail.com>
2025-10-18 23:08:15 +0600

    add dashboard blg route

--- src/app/(dashboard)/dashboard/projects/page.tsx
+++ src/app/(dashboard)/dashboard/projects/page.tsx
@@ -5,7 +5,7 @@ import ProjectCard from "@/components/modules/dashboard/ProjectCard";
 export default function ProjectPage() {
   return (
     <div>
-        <h1>ProjectPage</h1>
+        <h1>ProjectPage here</h1>
         <ProjectCard />
     </div>
   )
