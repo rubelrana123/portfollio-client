e64c0065f783017bfce4c6f902f8fc012104287a
rubelrana123 <rubelranatpi707458@gmail.com>
2025-10-20 23:52:01 +0600

    render card data

--- src/app/(dashboard)/dashboard/blogs/page.tsx
+++ src/app/(dashboard)/dashboard/blogs/page.tsx
@@ -1,6 +1,24 @@
  
-export default function BlogsPage() {
+import BlogCard from "@/components/modules/dashboard/BlogCard";
+
+ 
+export default  async function BlogsPage() {
+    const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/post`)
+  const posts = await data.json()
+  console.log(posts, "here post data all")
   return (
-    <div>BlogsPage</div>
+    <div>
+        <section className="bg-white dark:bg-gray-900">
+      <div className="container px-6 py-10 mx-auto">
+        <h1 className="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">
+          From the Blog
+        </h1>
+
+      {
+        posts.data.map((post: any) => <BlogCard key={post._id} post={post} />)
+      }
+       </div>
+    </section>
+    </div>
   )
 }
--- src/components/modules/dashboard/ProjectCard.tsx
+++ src/components/modules/dashboard/ProjectCard.tsx
@@ -1,27 +1,59 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Delete, Edit, Link } from "lucide-react";
 import Image from "next/image";
 
  
 export default function ProjectCard() {
   return (
-<div className="max-w-sm sm:max-w-md md:max-w-lg lg:max-w-2xs xl:max-w-2xl mx-auto border-2 border-amber-600">  
-    <div className="p-4">
-           <div className="flex justify-between">
-            <h2 className="text-2xl font-bold mb-2">Project Title</h2>
-            <p className="text-sm text-gray-600 mb-4">Draft</p>
-           </div>
-           <Image src="https://i.ibb.co/C2vd68H/screencapture-recycle-clothh-web-app-2022-12-04-15-47-03.png" alt="E-commerce PlatForm" width={300} height={180} className="object-cover" />
-        </div>
-        <div>
-            <h1>E-commerce PlatForm</h1>
-            <p>A full-stack e-commerce solution with payment integration and admin dashboard</p>
-            <p>Tech Stack: Next.js, Tailwind CSS, Node.js, Express, MongoDB, Stripe API</p>
-            <div>
-                <button>Edit Project</button>
-                <button>delete</button>  
-                <button>URL</button>
+ 
+<div className="flex justify-center items-center min-h-screen">
+    <div className="max-w-[720px] mx-auto">
+        <div className="relative flex flex-col text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
+            <div className="relative mx-4 mt-4 overflow-hidden text-gray-700 bg-white bg-clip-border rounded-xl h-96">
+                <Image
+                    src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=927&amp;q=80"
+                  
+                 alt="card-image" className="object-cover w-full h-full"
+                 height={500} width={500}
+                 />
+            </div>
+            <div className="p-6">
+                <div className="flex items-center justify-between mb-2">
+                    <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
+                        Apple AirPods
+                    </p>
+                    <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
+                        $95.00
+                    </p>
+                </div>
+                <p className="block font-sans text-sm antialiased font-normal leading-normal text-gray-700 opacity-75">
+                    With plenty of talk and listen time, voice-activated Siri access, and an
+                    available wireless charging case.
+                </p>
+                <div>
+                    <Badge className="mt-4">Headphones</Badge>
+                    <Badge className="mt-4 ml-2">Apple</Badge>  
+                    <Badge className="mt-4 ml-2">Accessories</Badge>
+                    <Badge className="mt-4 ml-2">Accessories</Badge>
+
+                </div>
+            </div>
+            <div className="flex justify-between p-6 pt-0">
+              <div>
+                <Edit /> 
+              </div>
+              <div>
+                <Delete /> 
+              </div>
+              <div>
+                <Link />
+              </div>
+ 
             </div>
         </div>
     </div>
+</div>
 
   )
 }
